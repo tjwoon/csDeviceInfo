4cf6aff7591e9627089831bb1d1fd0170b462824
TJ Woon <tjwoon@appxtream.com>
2018-05-25 15:35:45 +0800

    [Android] Delegate root checking entirely to RootBeer library

--- android/DeviceInfo.java
+++ android/DeviceInfo.java
@@ -23,44 +23,6 @@ public class DeviceInfo extends CordovaPlugin {
     static private final String ACTION_GETVERSIONNAME = "getVersionName";
     static private final String ACTION_ISHACKEDDEVICE = "isHackedDevice";
 
-    // Root checking config
-    static private final String[] ROOT_PACKAGES = {
-        "com.noshufou.android.su",
-        "com.thirdparty.superuser",
-        "eu.chainfire.supersu",
-        "com.koushikdutta.superuser",
-        "com.zachspong.temprootremovejb",
-        "com.ramdroid.appquarantine",
-        "david.lahuta.superuser.free.pro",
-    };
-    static private final String[] ROOT_FILES = {
-        "/system/bin/su",
-        "/system/xbin/su",
-        "/sbin/su",
-        "/system/su",
-        "/system/bin/.ext/.su",
-        "/system/usr/we-need-root/su-backup",
-        "/system/xbin/mu",
-    };
-    static private final String[] ROOT_DIRECTORYPERMISSIONS = {
-        "/",
-        "/data",
-        "/dev",
-        "/etc",
-        "/proc",
-        "/sbin",
-        "/system",
-        "/system/bin",
-        "/system/sbin",
-        "/system/xbin",
-        "/sys",
-        "/vendor/bin",
-    };
-    static private final String[] ROOT_COMMANDS = {
-        "su",
-        "busybox",
-    };
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
     throws JSONException
@@ -93,63 +55,12 @@ public class DeviceInfo extends CordovaPlugin {
 
     private void checkIfRooted (CallbackContext callbackContext)
     {
-        Boolean rootBeerRooted = false;
         RootBeer rootBeer = new RootBeer(cordova.getActivity().getApplicationContext());
-        if (rootBeer.isRootedWithoutBusyBoxCheck()) rootBeerRooted = true;
-
-        if(  rootBeerRooted
-          || hasRootPackages()
-          || hasRootFiles()
-          || hasRootPermissions()
-          || hasRootCommands()
-        ) {
+        if(rootBeer.isRootedWithoutBusyBoxCheck()) {
             callbackContext.success(1);
         } else {
             callbackContext.success(0);
         }
     }
 
-    private boolean hasRootPackages ()
-    {
-        Activity act = cordova.getActivity();
-        PackageManager packageManager = act.getPackageManager();
-        for(String pkg: ROOT_PACKAGES) {
-            try {
-                packageManager.getPackageInfo(pkg, 0);
-                return true;
-            } catch (PackageManager.NameNotFoundException e) {}
-        }
-        return false;
-    }
-
-    private boolean hasRootFiles ()
-    {
-        for(String path: ROOT_FILES) {
-            File f = new File(path);
-            if(f.exists()) return true;
-        }
-        return false;
-    }
-
-    private boolean hasRootPermissions ()
-    {
-        for(String path: ROOT_DIRECTORYPERMISSIONS) {
-            File f = new File(path);
-            if(f.canWrite()) return true;
-        }
-        return false;
-    }
-
-    private boolean hasRootCommands ()
-    {
-        Runtime runtime = Runtime.getRuntime();
-        for(String cmd: ROOT_COMMANDS) {
-            try {
-                Process p = runtime.exec(cmd);
-                p.destroy();
-                return true;
-            } catch (IOException e) {}
-        }
-        return false;
-    }
 }
